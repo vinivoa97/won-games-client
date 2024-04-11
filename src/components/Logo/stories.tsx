af99d62fb2098ca1f02861c9b7b51b26094dd65f
Vinicius Amaral <viniciuscopuesb@gmail.com>
2024-04-11 19:54:16 -0300

    feat: add logo component

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Logo from '.'
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as Meta
+
+export const Default: StoryObj = {}
